04d3e7d9953dccf4aa09cd7b29d9825e3cd0a0bd
irelia3 <belmontecarles@gmail.com>
2023-11-01 15:14:12 +0000

    Added tournaments2.py, it scrapes gol.gg players page to get a list of all the tournament and prints them

---
+++ src/tournaments2.py
@@ -0,0 +1,31 @@
+#### IMPORTS
+
+# basic
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+# select
+from selenium.webdriver.support.select import Select
+
+####
+
+#def playerstats(player, position, team, season, split):
+
+#def tournaments():
+url = "https://gol.gg/players/list/season-S13/split-ALL/tournament-ALL/"
+
+chrome_options = Options()
+chrome_options.add_argument("--incognito")
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get(url)
+
+driver.implicitly_wait(5) #Implicit wait is rarely the best solution, need to research a better way.
+
+tournament_box = driver.find_element(By.NAME, 'cbtournament')
+select = Select(tournament_box)
+tournament_list = select.options
+
+for t in tournament_list:
+	print(t.get_attribute("value"))
